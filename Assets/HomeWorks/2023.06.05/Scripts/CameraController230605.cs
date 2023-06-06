035d282ee3cb4b8ba48f2a1bdd8e780279a59ff4
Daighter <arkansiel@naver.com>
2023-06-06 23:29:57 +0900

    feat : 230605일자 과제

---
+++ Assets/HomeWorks/2023.06.05/Scripts/CameraController230605.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController230605 : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float zoomSpeed;
+    [SerializeField] float pading;
+
+    Vector2 moveDir;
+    private float zoomScroll;
+
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * moveDir.y * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.right * moveDir.x * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnPointer(InputValue value)
+    {
+        Vector2 mousePOs = value.Get<Vector2>();
+
+        if (mousePOs.x <= pading)
+            moveDir.x = -1;
+        else if (mousePOs.x >= Screen.width - pading)
+            moveDir.x = 1;
+        else
+            moveDir.x = 0;
+        
+        if (mousePOs.y <= pading)
+            moveDir.y = -1;
+        else if (mousePOs.y >= Screen.height - pading)
+            moveDir.y = 1;
+        else
+            moveDir.y = 0;
+    }
+
+    private void Zoom()
+    {
+        transform.Translate(Vector3.forward * zoomSpeed * zoomScroll * Time.deltaTime, Space.Self);
+    }
+
+    private void OnZoom(InputValue value)
+    {
+        zoomScroll = value.Get<Vector2>().y;
+    }
+}
---
+++ Assets/HomeWorks/2023.06.05/Scripts/EnemySpawner230605.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+
+public class EnemySpawner230605 : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float spawnTime;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
