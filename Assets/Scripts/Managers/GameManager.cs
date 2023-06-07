b81186d3fce16e585a12cbe83f21076163aeae61
Daighter <arkansiel@naver.com>
2023-06-07 21:09:16 +0900

    feat : 230607일자 수업내용

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,14 +9,16 @@ public class GameManager : MonoBehaviour
     public const string DefaultName = "Manager";
 
     private static GameManager instance;
-    private static DataManager dataManager;
     private static ResourceManager resourceManager;
+    private static DataManager dataManager;
     private static PoolManager poolManager;
+    private static UIManager uiManager;
 
     public static GameManager Instance { get { return instance; } }
-    public static DataManager DataManager { get { return dataManager; } }
     public static ResourceManager Resource { get { return resourceManager; } }
+    public static DataManager DataManager { get { return dataManager; } }
     public static PoolManager Pool { get { return poolManager; } }
+    public static UIManager UI { get { return uiManager; } }
 
     private void Awake()
     {
@@ -40,17 +42,20 @@ public class GameManager : MonoBehaviour
 
     private void InitManagers()
     {
-        // Manager init
-        GameObject dataObj = new GameObject() { name = "DataManager" };
-        dataObj.transform.SetParent(transform);
-        dataManager = dataObj.GetComponent<DataManager>();
-
         GameObject resourceObj = new GameObject() { name = "ResourceManager" };
         resourceObj.transform.SetParent(transform);
         resourceManager = resourceObj.AddComponent<ResourceManager>();
 
+        GameObject dataObj = new GameObject() { name = "DataManager" };
+        dataObj.transform.SetParent(transform);
+        dataManager = dataObj.GetComponent<DataManager>();
+
         GameObject poolObj = new GameObject() { name = "PoolManager" };
         poolObj.transform.SetParent(transform);
         poolManager = poolObj.AddComponent<PoolManager>();
+
+        GameObject uiObj = new GameObject() { name = "UIManager" };
+        uiObj.transform.SetParent(transform);
+        uiManager = uiObj.AddComponent<UIManager>();
     }
 }
---
+++ Assets/Scripts/UI/PopUpUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpUI : BaseUI
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    /*
+    public override void CloseUI()
+    {
+        base.CloseUI();
+
+        GameManager.UI.ClosePopUpUI();
+    }*/
+}
